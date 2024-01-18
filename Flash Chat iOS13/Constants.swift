829006f43f7333d31d8eae8dcff1425788f4b395
Jose Maria Vivanco <jm.vivlo@gmail.com>
2024-01-18 14:00:31 -0600

    Develop simple Chat App using Firebase

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Jose Vivanco on 25/01/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
---
+++ Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Jose Vivanco on 26/01/23.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
